1b2b0395f3826b48fa06f41983f603dff43e0b55
Vimal0204 <80668288+Vimal0204@users.noreply.github.com>
2023-06-24 16:23:03 +0530

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:google_fonts/google_fonts.dart';
+// import 'package:meals/data/dummy_data.dart';
+// import 'package:meals/screen/categories.dart';
+import 'package:meals/screen/tabs.dart';
+// import 'package:meals/screen/categories.dart';
+// import 'package:meals/screen/meals.dart';
+
+final theme = ThemeData(
+  useMaterial3: true,
+  brightness: Brightness.dark,
+  colorScheme: ColorScheme.fromSeed(
+    seedColor: const Color.fromARGB(255, 131, 57, 0),
+    brightness: Brightness.dark,
+  ),
+  textTheme: GoogleFonts.latoTextTheme(),
+);
+void main() {
+  runApp(const ProviderScope(child:  MyApp()));
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: theme,
+      home: const TabsScreen(),
+    );
+  }
+}
